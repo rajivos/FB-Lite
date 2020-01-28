df9ce73599c5c029d471c615dd5b900cfa9a112c
Rajiv Khushiram <rajivkhushiram@hotmail.com>
2020-01-29 04:54:39 +1100

    git ignores && Connection Made to database

--- fb-lite-mern/backend/server.js
+++ fb-lite-mern/backend/server.js
@@ -8,6 +8,9 @@ require ('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
@@ -15,12 +18,6 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
-
-
-
-
-app.use(cors());
-app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server is running on port : ${port}`);
